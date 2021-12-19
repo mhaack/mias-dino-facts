bc0ab076e8868cc204aa39f571ff7b4d082773b4
Markus Haack <markus.haack@gmail.com>
2021-12-19 18:04:38 +0100

    Optimize image generation

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 const Image = require('@11ty/eleventy-img');
 const util = require('util');
+const path = require('path')
 
 const emojiRegex = /[\u{1f300}-\u{1f5ff}\u{1f900}-\u{1f9ff}\u{1f600}-\u{1f64f}\u{1f680}-\u{1f6ff}\u{2600}-\u{26ff}\u{2700}-\u{27bf}\u{1f1e6}-\u{1f1ff}\u{1f191}-\u{1f251}\u{1f004}\u{1f0cf}\u{1f170}-\u{1f171}\u{1f17e}-\u{1f17f}\u{1f18e}\u{3030}\u{2b50}\u{2b55}\u{2934}-\u{2935}\u{2b05}-\u{2b07}\u{2b1b}-\u{2b1c}\u{3297}\u{3299}\u{303d}\u{00a9}\u{00ae}\u{2122}\u{23f3}\u{24c2}\u{23e9}-\u{23ef}\u{25b6}\u{23f8}-\u{23fa}]/ug
 
@@ -67,21 +68,28 @@ module.exports = config => {
 
     /* Shortcodes */
     const imageShortcode = async (src, className, alt, sizes) => {
-        let metadata = await Image(src.includes('http') ? src : `./src/${src}`, {
+        const metadata = await Image(src.includes('http') ? src : `./src/${src}`, {
             widths: [null, 400, 800, 1280],
             formats: ['webp', 'jpeg'],
             outputDir: './_site/img/dinos',
-            urlPath: '/img/dinos/'
+            urlPath: '/img/dinos/',
+            filenameFormat: function (id, src, width, format, options) {
+                const extension = path.extname(src)
+                const name = path.basename(src, extension)
+                return `${name}-${width}w.${format}`
+            }
         });
-    
+
         let imageAttributes = {
-            class: className,
             alt,
             sizes,
             loading: "lazy",
             decoding: "async"
         };
-    
+        if (className) {
+            imageAttributes.class = className
+        }
+        
         return Image.generateHTML(metadata, imageAttributes);
     }
 
